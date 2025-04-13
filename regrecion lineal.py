5e27dd5ef6cccdfe2248c5ff25b7db4ac152e339
Juandres0903 <103950866+Juandres0903@users.noreply.github.com>
2025-04-12 22:43:38 -0500

    Update regrecion lineal.py

--- regrecion lineal.py
+++ regrecion lineal.py
@@ -87,3 +87,29 @@ for col in categorical_cols:
         plt.title(f'Distribución de {col}')
         plt.xticks(rotation=45)
         plt.show()
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+df_clean = df.drop(columns=['name'], errors='ignore')
+
+for col in df_clean.columns:
+    if df_clean[col].dtype in ['float64', 'int64']:
+        df_clean[col] = df_clean[col].fillna(df_clean[col].median())
+    else:
+        df_clean[col] = df_clean[col].fillna(df_clean[col].mode()[0])
+
+df_encoded = pd.get_dummies(df_clean, drop_first=True)
+
+X = df_encoded.drop(columns=['Selling_Price'])
+y = df_encoded['Selling_Price']
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y, test_size=0.2, random_state=42)
+
+print("Dimensiones de entrenamiento:", X_train.shape)
+print("Dimensiones de prueba:", X_test.shape)
