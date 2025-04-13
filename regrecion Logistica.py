9f2db1b988f7ee117f29f3b754e914ad2014cc2c
Juandres0903 <103950866+Juandres0903@users.noreply.github.com>
2025-04-12 23:22:14 -0500

    Update regrecion Logistica.py

--- regrecion Logistica.py
+++ regrecion Logistica.py
@@ -25,6 +25,7 @@ plt.ylabel('Cantidad')
 plt.show()
 
 
+
 plt.figure(figsize=(12, 8))
 sns.heatmap(df.corr(), annot=True, cmap='coolwarm', fmt='.2f')
 plt.title("Matriz de correlación")
@@ -49,3 +50,43 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 print("Tamaño del conjunto de entrenamiento:", X_train.shape)
 print("Tamaño del conjunto de prueba:", X_test.shape)
+
+
+#regrecion logistica
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+log_reg = LogisticRegression(max_iter=1000)
+log_reg.fit(X_train, y_train)
+
+y_pred = log_reg.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+
+print(" Evaluación del modelo de Regresión Logística:")
+print(f"Accuracy:  {accuracy:.2f}")
+print(f"Precision: {precision:.2f}")
+print(f"Recall:    {recall:.2f}")
+print(f"F1-score:  {f1:.2f}\n")
+
+
+print("Reporte de clasificación:\n")
+print(classification_report(y_test, y_pred))
+
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+plt.figure(figsize=(6, 4))
+sns.heatmap(conf_matrix, annot=True, cmap="Blues", fmt='d')
+plt.title("Matriz de Confusión")
+plt.xlabel("Predicción")
+plt.ylabel("Valor Real")
+plt.xticks([0.5, 1.5], ['No Enfermo', 'Enfermo'])
+plt.yticks([0.5, 1.5], ['No Enfermo', 'Enfermo'], rotation=0)
+plt.show()
